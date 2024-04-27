3856376f02991cb8103823575518647b629851dc
Thomas Noel <thomas.c.noel@gmail.com>
2024-04-27 16:08:06 -0700

    Finish up dotdrop; add readme

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # dotdrop
-A dropdown menu utility
+
+A simple dropdown menu utility.
+
+---
+## Overview
+
+Dotdrop is a dropdown menu utility designed to be simple and efficient. The 
+tool relies on notions of *dots* and *drops*. *Dots* are HTML elements
+that, when clicked, reveal a dropdown menu. A *drop* refers to the container
+element that holds the dropdown menu and its entries.
+
+---
+## Usage
+
+Each dotdrop instance corresponds to one ```dot-n-container```. The ```n``` 
+refers to the user-designated id given to a container. Note that each of
+these ids must be unique. Inside of each ```dot-n-container``` element, there 
+should be a child element with class ```dot-n``` containing the dot that the 
+user will click to trigger the drop. See the example below (remember ```n``` 
+should be replaced with a unique id).
+
+```html
+<body>
+  ...
+  <div class="dot-n-container">
+    <div class="dot-n">
+      <!-- Your dot of choice -->
+    </div>
+  </div>
+  ...
+</body>
+```
+
+After ```dot-n-container``` elements have been configured, they can be attached
+to a ```DotDrop``` instance. ```DotDrop``` is instantiated with a single
+argument, the ```dot-n``` class selector.
+
+```javascript
+const ddn = new DotDrop('.dot-n');
+```
+
+To add entries and attach it to a callback, simply call ```addDropEntry``` on 
+the dotdrop instance. For example, if we wanted to add an entry that makes the
+background color of the screen green (who wouldn't want to do that??), then
+we would run the code that follows.
+
+```javascript
+ddn.addDropEntry("Turn Screen Green", (e) => {
+  let body = document.querySelector("body");
+  if (body.style.backgroundColor == "green") {
+    body.style.backgroundColor = "white";
+  } else {
+    body.style.backgroundColor = "green";
+  }
+});
+```
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 const html = require('./index.html');
 
+/**
+  * A simple dropdown menu utility.
+  */
 class DotDrop {
-  constructor(dotSelector, dropSelector) {
+  static ids = [];
+
+  /**
+    * @param {!string} dotSelector The dot selector.
+    */
+  constructor(dotSelector) {
+    this.id = +dotSelector.split('-')[1]; 
+    if (this.id in DotDrop.ids) {
+      throw Error("Invalid dotSelector: Ensure dot number isn't already taken");
+    }
+
+    DotDrop.ids.push(this.id);
+
     this.toggle_on = false;
     this.dot = document.querySelector(dotSelector);
+    if (!this.dot) {
+      throw Error("Invalid dotSelector: Ensure that specified element already exists");
+    }
+
+    this.container = document.querySelector(`.dot-${this.id}-container`);
 
     // Make dots an obviously clickable thing
     this.dot.style.cursor = "pointer";
 
-    this.drop = document.querySelector(dropSelector);
+    this.drop = document.createElement("div");
+    this.container.append(this.drop);
+
+    this.entries = [];
+    
+    // Assumes dotSelector follows the "dot-n" naming convention
+    this.drop.classList.add(`drop-${this.id}`);
+    this.drop.style.display = "none";
 
     this.dot.addEventListener("click", (e) => {
       // Update state
@@ -20,15 +47,75 @@ class DotDrop {
         this.drop.style.display = "none";
       }
     });
+
+    this.drop.addEventListener("mouseleave", (e) => {
+      if (this.toggle_on) {
+        this.toggle_on = !this.toggle_on;
+        this.drop.style.display = "none";
+      }
+    });
+
+  }
+
+  /**
+    * Adds an entry to the drop.
+    *
+    * @param {!string} text The text in the entry.
+    * @param {!function(obj)} cb Callback executing action on entry click
+    */
+  addDropEntry(text, cb) {
+    let entry = document.createElement("div");
+    entry.classList.add("entry");
+
+    entry.innerText = text;
+    this.drop.append(entry); 
+
+    // Specify what to do when entry is clicked
+    entry.addEventListener("click", cb);
+    this.entries.push(entry);
   }
 }
 
 (() => {
-  // attach event listener
-  // Can optionally
-  // let dots = document.querySelectorAll('[class^=dot-]');
-  // let drops = document.querySelectorAll('[class^=drop-]');
+  // Example, instantiating and specifying the behavior of a dotdrop.
+  // pass in the selector.
+  const dd1 = new DotDrop('.dot-1');
 
-  const dd = new DotDrop('.dot-1', '.drop-1');
+  dd1.addDropEntry("Turn Screen Green", (e) => {
+    let body = document.querySelector("body");
+    if (body.style.backgroundColor == "green") {
+      body.style.backgroundColor = "white";
+    } else {
+      body.style.backgroundColor = "green";
+    }
+  });
 
+  dd1.addDropEntry("Do Nothing", (e) => {});
+
+  dd1.addDropEntry("Toggle Greeting", (e) => {
+    let greeting = document.getElementById("greeting");
+    if (!greeting) {
+      let body = document.querySelector("body");
+      let greeting = document.createElement("div");
+      greeting.id = "greeting";
+      greeting.innerText = "Hello, Gerald";
+      body.append(greeting);
+    } else {
+      greeting.remove();
+    }
+  });
+
+  // Instantiating a second dotdrop
+  const dd2 = new DotDrop('.dot-2');
+  
+  dd2.addDropEntry("Turn Screen Red", (e) => {
+    let body = document.querySelector("body");
+    if (body.style.backgroundColor == "red") {
+      body.style.backgroundColor = "white";
+    } else {
+      body.style.backgroundColor = "red";
+    }
+  });
 })();
+
+export default DotDrop;
